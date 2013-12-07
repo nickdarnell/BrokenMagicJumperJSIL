21f3c7e607e03b9fb7a9dc12ac36704dcfb88f6c
Nick Darnell <NickDarnell@gmail.com>
2013-12-07 15:49:31 -0500

    Adding JSIL compiled game.

---
+++ Game/Content/Levels/classical_ruin_tiles_64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="classical_ruin_tiles_64x64" tilewidth="64" tileheight="64">
+ <image source="../LevelSprites/classical_ruin_tiles_64x64.png" width="1504" height="1024"/>
+</tileset>
---
+++ Game/Content/Levels/metatiles64x64.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="metatiles64x64" tilewidth="64" tileheight="64">
+ <image source="../LevelSprites/metatiles64x64.png" width="320" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+</tileset>
